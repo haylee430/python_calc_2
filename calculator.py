4c7d8bcc61d0f70f9eb13e6212c831cfbcf61456
vagrant <vagrant@vagrant.vm>
2020-09-30 21:16:27 +0000

    added add, sub, and mult functions

--- calculator.py
+++ calculator.py
@@ -5,3 +5,24 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+while True:
+
+    user_input = input("Enter your equation:")
+    token = user_input.split(" ") 
+    #print(token) 
+ 
+    if(token[0] == "q"): 
+        break 
+    else:
+        if(token[0] == 'add'):
+            result = add(int(token[1]), int(token[2]))
+            print(result)
+        elif(token[0] == 'subtract'):
+            result = subtract(int(token[1]), int(token[2]))
+            print(result)
+        elif(token[0] == 'multiply'):
+            result = multiply(int(token[1]), int(token[2]))
+            print(result)
+        
+        
+
